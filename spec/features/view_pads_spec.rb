85f9a26bb034deff13ee3555b3f6835e56d29544
Pete Wilkins <pete.p.wilkins@gmail.com>
2017-04-18 15:05:29 +0100

    passing first test, user can view pads page

--- spec/features/view_pads_spec.rb
+++ spec/features/view_pads_spec.rb
@@ -1,5 +1,5 @@
 feature 'View the pads' do
-  scenario 'User can view all pads' do
+  scenario 'User can visit pads page' do
     visit '/pads'
     expect(page).to have_content('Book a pad')
   end
