2709404ba778f8d45e899613dc541568061971eb
Naz <nazmalik08@gmail.com>
2017-04-21 11:06:43 +0100

    Lord can view bookings made to their pads in kennel

--- spec/features/kennel_spec.rb
+++ spec/features/kennel_spec.rb
@@ -5,4 +5,17 @@ feature 'view kennel' do
     click_button 'My Kennel'
     expect(page).to have_content('House by the Sea')
   end
+
+  scenario 'dog-lord can view bookings for their pads' do
+    list_pad
+    sign_up_owner
+    click_button 'More Info'
+    fill_in 'booking_date', with: "2017/05/01"
+    click_button 'Confirm Request'
+    click_button 'Sign out'
+    sign_in
+    click_button 'My Kennel'
+    expect(page).to have_content '2017/05/01'
+  end
+
 end
