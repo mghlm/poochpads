a17464d1945a30e0b5e5d7fc8b7e16b8bddf6c0e
kittysquee <kittysquee@gmail.com>
2017-04-21 10:59:15 +0100

    Adds booked dates to request page

---
+++ spec/features/request_spec.rb
@@ -0,0 +1,11 @@
+feature 'request pad' do
+  scenario 'Lists booked dates for pad' do
+    list_pad
+    sign_up_owner
+    click_button('More Info')
+    fill_in(:booking_date, with: '21/04/2017')
+    click_button('Confirm Request')
+    visit('/pads/request')
+    expect(page).to have_content('21/04/2017')
+  end
+end
